39ecbfccb0e772c64324c5bb983e01bb772128b5
alexbertschi <alex.bertschi@gmail.com>
2022-09-21 17:24:52 +0200

    Frau Gubelmaa

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          See how it runs in the background
+          Frau Gubelmaaaaa, Frau Gubelmaaaaaa
         </p>
       </div>
     );
