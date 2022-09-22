4e15776e329443b04ade2c68a8c0c99529b62edd
alexbertschi <alex.bertschi@gmail.com>
2022-09-22 13:09:19 +0200

    added jest config file

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testMatch: [
+      '**\\*test.tsx',
+      '**/*test.tsx',
+      'src\\**\\*.test.tsx',
+      'src/**/*.test.tsx',
+      '<rootDir>\\src\\**\\*.test.tsx',
+      '<rootDir>/src/**/*.test.tsx',
+      'src/.*|(.|/)(.test).tsx?$'
+    ],
+  };
